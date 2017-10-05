c8af8f43b5f0f53c59423ed0dfd5daabfa2f4b1d
Wen <wfwuestc@163.com>
2017-10-05 17:42:14 +0800

    add script server watch

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,10 @@
-const path = require('path')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+var path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new UglifyJSPlugin(),
-  ],
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015'],
-          },
-        }],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-
-}
+    path: path.resolve(__dirname, 'dist')
+  }
+};
