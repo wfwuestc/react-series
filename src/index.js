c3ae9e8665285b3e0a878bca0fd4f037f0562d38
Wen <wfwuestc@163.com>
2017-10-05 17:28:56 +0800

    watch server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import _ from 'lodash';
-import $ from 'jquery'
 var log = console.log.bind(console)
-function component() {
-  var element = $('<div></div>')
-  element.html(_.join(['Hello', 'webpack'], ' '))
-  log(element)
+import _ from 'lodash'
+import j from 'jquery'
+import foo from './foo'
+
+function component () {
+  var element = j('<div></div>');
+
+  /* lodash is required for the next line to work */
+  element.html(_.join(['Hi','webpack'], ' '))
+
   return element.get(0);
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
+console.log(foo)
+console.log(foo()) //打开浏览器控制台看看结果
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
-const path = require('path');
+const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new UglifyJSPlugin(),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015'],
+          },
+        }],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+}
