d58566a907ece88bfe5f0d6ea8d2cf974853ee43
Wen <wfwuestc@163.com>
2017-10-06 18:10:30 +0800

    完成构架，将输入框变为TodoInput组件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Welcome} from './Welcome'
-
+import App from './App'
 
 ReactDOM.render(
-    <Welcome name="wen"/>,
+    <App />,
     document.getElementById('root')
 )
